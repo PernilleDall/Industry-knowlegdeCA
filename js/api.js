42f082bafc3e06c5678827d5a5c84cf526d17831
PernilleDall <pernille.dall93@gmail.com>
2024-01-19 12:07:26 +0100

    updated api

--- js/api.js
+++ js/api.js
@@ -16,11 +16,11 @@ export const projects = [
   },
   {
     id: 1,
-    title: "GODIS - Prosjekteksamen",
-    text: "Prosjekteksamen hvor vi i sammarbeid med Hennig Olsen skulle lage en ny små-is for dem. En utrolig gøy oppgave hvor det å møte en ekte “kunde” var en del av oppgaven. Produktet ble en is ved navn “GODIS”, en fargerik og spennende is som passer perfekt til en varm sommerdag men også en lørdag midt på mørke høsten.",
+    title: "Forsidebilde til magasin - Fotooppgave",
+    text: "I dette prosjektet skulle jeg ende opp med et portrettbilde som cover på magasinet i-D. Øyet måtte enten skjules eller lukkes. Jeg måtte både fotografere i naturlig lys men også med studio lys. ",
     image: [
       {
-        url: "../images/hennigolsen-main.jpg",
+        url: "../images/id-main.jpg",
         alt: "Hen1n",
       },
       {
